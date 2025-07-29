568dd55dae57364bcaa8577648feaeb7b26c7715
Bluefox_192 <bluefox192@localhost.localdomain>
2025-07-29 23:13:12 +0700

    adding repository for my web dev journey
    adding it for documentation of my projet from the course

---
+++ test file/javascript/test.js
@@ -0,0 +1,12 @@
+function add7(number){
+    return number + 7;
+}
+function multiply(a, b){
+    return a * b;
+}
+function capitalize(string){
+    return string [0];
+}
+function lastLetter(last){
+    return last.slice(-1);
+}
