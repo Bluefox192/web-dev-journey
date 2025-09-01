a29697123852f9a542117963cbc5b5c3058fc787
Bluefox192 <105058251+Bluefox192@users.noreply.github.com>
2025-09-01 22:12:03 +0700

    Update test.js
    daily exercises

--- test file/javascript/test.js
+++ test file/javascript/test.js
@@ -1,12 +1,28 @@
-function add7(number){
-    return number + 7;
-}
-function multiply(a, b){
-    return a * b;
-}
-function capitalize(string){
-    return string [0];
-}
-function lastLetter(last){
-    return last.slice(-1);
-}
+let hello = "Hello, Blu!";
+let nama = prompt("masukkan nama: ");
+let umur = prompt("masukkan umur: ");
+const halo = "Halo, nama saya " + nama + " dan umur saya " + umur;
+for (let i = 1; i <= 10; i++){
+    const numbers = i;
+};
+function factorial(n){
+    let result = 1;
+    for(let i = 2; i <= n; i++){
+        result *= i;
+    }
+    return result;
+};
+let hobi = ['gaming', 'coding', 'mancing', 'makan', 'baca'];
+function looping(array){
+    const start = array.length - 1;;
+    for(let i = 0; i <= start; i++){
+    let result = array[i];
+    console.log(result);
+    };
+};
+let angka = [1, 2 ,3, 4, 5];
+const mapped = angka.map((haha) => {for(let i = 0; i <= 4; i++){
+    let kuadratkan = haha[i] ** 2;
+    return kuadratkan;
+}});
+console.log(mapped);
