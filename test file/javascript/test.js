8e37cde3a538f41572c8cf32c8dc9309f6140f31
Bluefox192 <105058251+Bluefox192@users.noreply.github.com>
2025-09-08 23:42:48 +0700

    Update test.js
    exercising project

--- test file/javascript/test.js
+++ test file/javascript/test.js
@@ -1,12 +1,31 @@
-let hello = "hello";
+const prompt = require("prompt-sync")();
 
-function splitter (str){
-    return str.split('').reverse().join('');
+const deposit = () => {
+    while(true){
+      const depositAmount = prompt("Enter a deposit amount: ");
+      const numberDepositAmount = parseFloat(depositAmount);
+  
+      if (isNaN(numberDepositAmount) || numberDepositAmount <= 0){
+          console.log("Inavalid deposit amount, try again");
+      }else{
+        return numberDepositAmount;
+      }
+   }
+};
+
+
+const getNumberOfLines = () =>{
+    while(true){
+      const lines = prompt("Enter the number of lines to bet on(1-3): ");
+      const numberOfLines = parseFloat(lines);
+  
+      if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3){
+          console.log("Inavalid number of lines, try again");
+      }else{
+        return numberOfLines;
+      }
+   }
 }
 
-function palindromeCheck(str){
-    const cleanedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
-    const reversedStr = cleanedStr.split('').reverse().join('');
-    return cleanedStr === reversedStr;
-}
-
+const depositAmount = deposit();
+const numberOfLines = getNumberOfLines();
