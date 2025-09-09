915762a3626e27d53d84be75142238ff3f92a701
Bluefox192 <105058251+Bluefox192@users.noreply.github.com>
2025-09-09 20:50:20 +0700

    Update test.js
    adding bet

--- test file/javascript/test.js
+++ test file/javascript/test.js
@@ -1,5 +1,24 @@
 const prompt = require("prompt-sync")();
 
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+  "A": 2,
+  "B": 4,
+  "C": 6,
+  "D": 8
+}
+
+const SYMBOLS_VALUE = {
+  "A": 5,
+  "B": 4,
+  "C": 3,
+  "D": 2
+}
+
+
+
 const deposit = () => {
     while(true){
       const depositAmount = prompt("Enter a deposit amount: ");
@@ -27,5 +46,31 @@ const getNumberOfLines = () =>{
    }
 }
 
-const depositAmount = deposit();
+const getBet = (balance, lines) =>{
+      while(true){
+      const bet = prompt("Enter the bet per line: ");
+      const numberBet = parseFloat(bet);
+  
+      if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines){
+          console.log("Inavalid bet, try again");
+      }else{
+        return numberBet;
+      }
+   }
+}
+
+const spin = () =>{
+  const symbols = [];
+  for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)){
+    for (let i = 0; i < count; i++){
+      symbols.push(symbol);
+    }
+  }
+  console.log(symbols);
+}
+
+spin();
+
+let balance = deposit();
 const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
