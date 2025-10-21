023e05bdf3509658f92a8440f54b7f3b8156b81f
Bluefox192 <105058251+Bluefox192@users.noreply.github.com>
2025-10-21 23:25:18 +0700

    Update script.js
    1 line a day

--- freecodecamp/javascript/Building-a-calorie-counter/script.js
+++ freecodecamp/javascript/Building-a-calorie-counter/script.js
@@ -1,29 +1,45 @@
-const calorieCounter = document.getElementById('calorie-counter');
-const budgetNumberInput = document.getElementById('budget');
-const entryDropdown = document.getElementById('entry-dropdown');
-const addEntryButton = document.getElementById('add-entry');
-const clearButton = document.getElementById('clear');
-const output = document.getElementById('output');
-let isError = false;
+Skip to content
+Profile
 
-function cleanInputString(str) {
-  const regex = /[+-\s]/g;
-  return str.replace(regex, '');
-}
+    JavaScript Algorithms and Data Structures
+    Learn Form Validation by Building a Calorie Counter
 
-function isInvalidInput(str) {
-  const regex = /\d+e\d+/i;
-  return str.match(regex);
-}
+script.js
+19
+20
+21
+22
+23
+24
+Step 52
 
-function addEntry() {
-  const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
-  const HTMLString = `
-  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
-  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
-  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>`;
-}
+To see your new HTML content for the targetInputContainer, you will need to use the innerHTML property.
 
-console.log (HTMLString)
-;
+The innerHTML property sets or returns the HTML content inside an element.
+
+Here is a form element with a label and input element nested inside.
+Example Code
+
+<form id="form">
+  <label for="first-name">First name</label>
+  <input id="first-name" type="text">
+</form>
+
+If you want to add another label and input element inside the form, then you can use the innerHTML property as shown below:
+Example Code
+
+const formElement = document.getElementById("form");
+const formContent = `
+  <label for="last-name">Last name</label>
+  <input id="last-name" type="text">
+`;
+formElement.innerHTML += formContent;
+
+Use the addition assignment operator += to append your HTMLString variable to targetInputContainer.innerHTML.
+
+
+/**
+* Your test output will go here
+*/
+
+Navigated to Step 52
